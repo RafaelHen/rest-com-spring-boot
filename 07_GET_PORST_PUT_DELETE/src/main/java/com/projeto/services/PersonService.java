6773b708ed2651041e3f696fe0c130aba6b12251
Rafael Victor de Oliveira <rafaelvictor368@hotmail.com>
2022-09-26 22:34:10 -0300

    Verbos HTTP GET,POST,PUT e DELETE

--- 07_GET_PORST_PUT_DELETE/src/main/java/com/projeto/services/PersonService.java
+++ 07_GET_PORST_PUT_DELETE/src/main/java/com/projeto/services/PersonService.java
@@ -1,5 +1,8 @@
 package com.projeto.services;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -14,9 +17,20 @@ public class PersonService {
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
 			
 	
+	public List<Person> findAll() {
+		logger.info("Finding all person!");
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+	
 	public Person findById(String id) {
 		
-		logger.info("Buscando uma pessoa!");
+		logger.info("Finding one person!");
 		
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
@@ -24,6 +38,33 @@ public class PersonService {
 		person.setSobrenome("Paulo");
 		person.setEndereco("São Paulo");
 		person.setGenero("Homem");
+		return person;
+	}
+	
+	public Person create(Person person) {
+		logger.info("Creating one person!");
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one person!");
+		
+		return person;
+	}
+	
+	public void delete(String id) {
+		logger.info("Deleting one person!");
+	}
+	
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setNome("Person name" + i);
+		person.setSobrenome("Last Name" + i);
+		person.setEndereco("Some adress in Brazil" + i);
+		person.setGenero("Male");
 		return person;
 	}
 }
