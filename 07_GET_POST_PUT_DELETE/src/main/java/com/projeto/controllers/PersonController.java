0222fc4467518e56d92e1c91bf0b846f1284f37d
Rafael Victor de Oliveira <rafaelvictor368@hotmail.com>
2022-11-03 19:17:54 -0300

    Padrao de Projetos VO

--- 07_GET_POST_PUT_DELETE/src/main/java/com/projeto/controllers/PersonController.java
+++ 07_GET_POST_PUT_DELETE/src/main/java/com/projeto/controllers/PersonController.java
@@ -4,47 +4,55 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projeto.model.Person;
+import com.projeto.data.vo.v1.PersonVO;
 import com.projeto.services.PersonService;
 
 @RestController
 @RequestMapping("/person")
-public class PersonController {
+public class PersonController  {
 
 	@Autowired
 	private PersonService service;
 	
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id) throws Exception {
+	
+	@GetMapping(value = "/{id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
 		return service.findById(id);
 	}
 
-	@RequestMapping(method = RequestMethod.POST,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person Create(@RequestBody Person person) throws Exception {
-		return service.create(person);
-	}
-		@RequestMapping(method = RequestMethod.PUT,
-				consumes = MediaType.APPLICATION_JSON_VALUE,
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 				produces = MediaType.APPLICATION_JSON_VALUE)
-		public Person Update(@RequestBody Person person) throws Exception {
-			return service.update(person);
+	public PersonVO Create(@RequestBody PersonVO PersonVO) throws Exception {
+		return service.create(PersonVO);
 	}
 	
-		@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-		public void delete(@PathVariable(value = "id") String id){
-		}	
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+				produces = MediaType.APPLICATION_JSON_VALUE)
+		public PersonVO Update(@RequestBody PersonVO PersonVO) throws Exception {
+			return service.update(PersonVO);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 }
--- 07_GET_POST_PUT_DELETE/src/main/java/com/projeto/services/PersonService.java
+++ 07_GET_POST_PUT_DELETE/src/main/java/com/projeto/services/PersonService.java
@@ -1,70 +1,79 @@
 package com.projeto.services;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projeto.data.vo.v1.PersonVO;
+import com.projeto.exception.ResourceNotFoundExcepetion;
+import com.projeto.mapper.DozerMapper;
 import com.projeto.model.Person;
+import com.projeto.repositories.PersonRepository;
 
 @Service
 public class PersonService {
-	
-	private final AtomicLong counter = new AtomicLong();
+
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
-			
-	
-	public List<Person> findAll() {
-		logger.info("Finding all person!");
-		List<Person> persons = new ArrayList<Person>();
-		for (int i = 0; i < 8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
-		
-		return persons;
+
+	@Autowired
+	PersonRepository repository;
+
+	public List<PersonVO> findAll() {
+		logger.info("Finding all PersonVO!");
+
+		return DozerMapper.parseListObject(repository.findAll(), PersonVO.class);
 	}
-	
-	public Person findById(String id) {
-		
-		logger.info("Finding one person!");
-		
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setNome("Marcos");
-		person.setSobrenome("Paulo");
-		person.setEndereco("São Paulo");
-		person.setGenero("Homem");
-		return person;
+
+	public PersonVO findById(Long id) {
+
+		logger.info("Finding one PersonVO!");
+
+		var Entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundExcepetion("No records found by ID!"));
+		return DozerMapper.parseObject(Entity, PersonVO.class);
 	}
-	
-	public Person create(Person person) {
+
+	public PersonVO create(PersonVO person) {
 		logger.info("Creating one person!");
-		
-		return person;
+		var entity = DozerMapper.parseObject(person, Person.class);
+		var vo =  DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
 	}
-	
-	public Person update(Person person) {
-		logger.info("Updating one person!");
-		
-		return person;
+
+	public PersonVO update(PersonVO PersonVO) {
+		logger.info("Updating one PersonVO!");
+
+		var entity = repository.findById(PersonVO.getId())
+				.orElseThrow(() -> new ResourceNotFoundExcepetion("No records found by ID!"));
+
+		entity.setNome(PersonVO.getNome());
+		entity.setSobrenome(PersonVO.getSobrenome());
+		PersonVO.setEndereco(PersonVO.getEndereco());
+		entity.setGenero(PersonVO.getGenero());
+
+		var vo =  DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
 	}
-	
-	public void delete(String id) {
-		logger.info("Deleting one person!");
+
+	public void delete(Long id) {
+		logger.info("Deleting one PersonVO!");
+
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundExcepetion("No records found by ID!"));
+
+		repository.delete(entity);
 	}
-	
-	private Person mockPerson(int i) {
-		
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setNome("Person name" + i);
-		person.setSobrenome("Last Name" + i);
-		person.setEndereco("Some adress in Brazil" + i);
-		person.setGenero("Male");
-		return person;
-	}
+
+//	
+//	private PersonVO mockPerson(int i) {
+//		
+//		Person person = new Person();
+//		person.setId(counter.incrementAndGet());
+//		person.setNome("Person name" + i);
+//		person.setSobrenome("Last Name" + i);
+//		person.setEndereco("Some adress in Brazil" + i);
+//		person.setGenero("Male");
+//		return person;
+//	}
 }
