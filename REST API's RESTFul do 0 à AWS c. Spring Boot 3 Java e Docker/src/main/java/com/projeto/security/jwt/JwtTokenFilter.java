a9e7d18c6a75455dfceb081c800c3e4de3a8b994
RafaelHen <rafaelvictor368@hotmail.com>
2024-03-02 23:52:22 -0300

    Implementando JwtTokenFilter e JwtConfigurer 2/2

--- REST API's RESTFul do 0 à AWS c. Spring Boot 3 Java e Docker/src/main/java/com/projeto/security/jwt/JwtTokenFilter.java
+++ REST API's RESTFul do 0 à AWS c. Spring Boot 3 Java e Docker/src/main/java/com/projeto/security/jwt/JwtTokenFilter.java
@@ -1,5 +1,40 @@
 package com.projeto.security.jwt;
 
-public class JwtTokenFilter {
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class JwtTokenFilter extends GenericFilterBean{
+
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
+	
+	public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+		super();
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String token = jwtTokenProvider.resolveToken((HttpServletRequest)request);
+		if (token != null && jwtTokenProvider.validadeToken(token) ) {
+			Authentication auth = jwtTokenProvider.getAuthentication(token);
+			if (auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+				
+			}
+		}
+		chain.doFilter(request, response);
+	}
 
 }
